da63a5a622fec30add5075b421e95cb641a9a345
Prajwal Gowda S <65961288+Prajwal518@users.noreply.github.com>
2021-02-13 12:56:56 +0530

    Expressions you can add the const or var using this"{}"

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const name = "Prajwal";
+const number = 5;
+ReactDOM.render(
+  <div>
+    <h1>Hello {name}!</h1>
+    <p>
+      My lucky number is {number} Or it could be
+      {Math.floor(Math.random() * 10)}
+    </p>
+  </div>,
+  document.getElementById("root")
+);
